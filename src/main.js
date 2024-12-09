1d279a39d12d262d6d1ce6a7beae2b30d190a009
Raihan Nooriman <noorimanr@gmail.com>
2024-12-09 22:37:09 +0800

    contact api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { web } from "./application/web.js";
-import { logger } from "./application/logging.js";
+import { web } from "./app/web.js";
+import { logger } from "./app/logging.js";
 
 web.listen(3000, () => {
   logger.info('Server is running on port 3000');
